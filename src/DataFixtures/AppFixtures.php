be39cfd87145c072248a20bcae99995167d34119
lopesf-a <francois.lopes@yahoo.com>
2020-09-21 17:11:41 +0200

    Commit 3 Ads + affichage Base de Donnée

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use App\Entity\Ad;
 use Faker\Factory;
+use App\Entity\Image;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 
@@ -13,24 +14,35 @@ class AppFixtures extends Fixture
     {
         $faker = Factory::create('fr-FR');
 
-        for($i =1; $i <= 30; $i++){
-        $ad = new Ad();
+        for($i =1; $i <= 30; $i++)
+        {
+            $ad = new Ad();
 
-        $title        = $faker->sentence();
-        $coverImage   = $faker->imageUrl(1000,350);
-        $introduction = $faker->paragraph(2);
-        $content      = '<p>'. join('</p><p>', $faker->paragraphs(5)). '</p>';
+            $title        = $faker->sentence();
+            $coverImage   = $faker->imageUrl(1000,350);
+            $introduction = $faker->paragraph(2);
+            $content      = '<p>'. join('</p><p>', $faker->paragraphs(5)). '</p>';
 
-        
-        $ad->setTitle($title)
-                ->setCoverImage($coverImage)
-                ->setIntroduction($introduction)
-                ->setContent($content)
-                ->setPrice(mt_rand(40, 200))
-                ->setRooms(mt_rand(1,5));
             
-        $manager -> persist($ad); 
-    } 
+            $ad->setTitle($title)
+                    ->setCoverImage($coverImage)
+                    ->setIntroduction($introduction)
+                    ->setContent($content)
+                    ->setPrice(mt_rand(40, 200))
+                    ->setRooms(mt_rand(1,5));
+            
+            for($j = 1; $j <= mt_rand(2,5); $j++){
+                    $image = new Image();
+
+                    $image->setUrl($faker->imageUrl())
+                            ->setCaption($faker->sentence())
+                            ->setAd($ad);
+
+                    $manager->persist($image);        
+                }
+
+                $manager -> persist($ad); 
+        } 
 
         $manager->flush();
     }
